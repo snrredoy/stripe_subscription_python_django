5fdd7721ac2f9e5c6fc5b1e98a076ae922148c22
snrredoy <snrredoy3@gmail.com>
2025-05-05 15:29:57 +0600

    add package model and set it in admin

--- subscription/admin.py
+++ subscription/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Package
 
 # Register your models here.
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'interval', 'stripe_product_id', 'stripe_price_id', 'discount', 'discount_price', 'is_active')
+    list_display_links = ('id', 'name',)
--- subscription/models.py
+++ subscription/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+SUBSCRIPTION_TYPE = (
+    ('year', 'Yearly'),
+    ('month', 'Monthly'),
+    ('week', 'Weekly'),
+    ('daily', 'Daily')
+)
+class Package(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    interval = models.CharField(max_length=10, choices=SUBSCRIPTION_TYPE, default='month')
+
+    stripe_product_id = models.CharField(max_length=100, blank=True)
+    stripe_price_id = models.CharField(max_length=100, blank=True)
+
+    discount = models.DecimalField(help_text='Set discount percentages.',max_digits=10, decimal_places=2, default=0)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    def get_discount_price(self):
+        return self.price - (self.price * self.discount / 100)
+    
+    def save(self, *args, **kwargs):
+        if self.discount > 0 and self.discount_price >= 0:
+            self.discount_price = self.get_discount_price()
+        else:
+            self.discount_price = self.price
+        super().save(*args, **kwargs)
+
