8da4bb6164980c7bf0ecf623731c006e503a63f6
snrredoy <snrredoy3@gmail.com>
2025-05-06 10:32:57 +0600

    add subscription model

--- subscription/models.py
+++ subscription/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -35,3 +36,16 @@ class Package(models.Model):
             self.discount_price = self.price
         super().save(*args, **kwargs)
 
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscription_user')
+    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name='subscription_package')
+
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+
+    stripe_subscription_id = models.CharField(max_length=100, blank=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.email} - {self.package.name}'
